657b18eb883aff0b8bf124b8405f6aeff433b883
dakota <dakotaslabb@gmail.com>
2019-10-07 18:55:04 -0400

    first commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Pinball.Models;
+
+namespace Pinball.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        public IActionResult Register()
+        {
+            return View(); 
+        }
+        public IActionResult Result(User u)
+        {
+            ViewBag.FirstName = u.FirstName;
+            ViewBag.LastName = u.LastName;
+            ViewBag.Email = u.Email;
+            ViewBag.Password = u.UserPassword;
+            ViewBag.Phone = u.PhoneNumber;
+            return View(u);
+        }
+
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Models/User.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Pinball.Models
+{
+    public class User
+    {
+        [Required][StringLength(30, MinimumLength = 1)]
+        public string FirstName { get; set; }
+
+        [Required][StringLength(30, MinimumLength = 1)]
+        public string LastName { get; set; }
+
+        [Required]
+        public string Email { get; set; }
+
+        [Required][MaxLength(30)][MinLength(5)]
+        public string UserPassword { get; set; }
+
+        [Required]
+        public int PhoneNumber { get; set; }
+    }
+}
